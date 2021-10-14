80fa9f3dc256c408812e57824c8a5a3ca7e55352
koresachin7 <koresachin7@gmail.com>
2021-10-14 22:38:48 +0530

    Added : - Analyse the Mood Sad Or Happy

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -5,7 +5,15 @@ package com.bridgelabz;
  * @version - 16.0
  */
 public class MoodAnalyser {
+    public static String analyseMood(String message) {
+        if (message.contains("sad"))
+            return "sad";
+        else
+            return "Happy";
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Mood Analyser");
+        String message ="i am sad";
+        System.out.println(analyseMood(message));
     }
 }
