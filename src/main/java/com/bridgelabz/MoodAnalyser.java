10c14c7a5327b18b8e5336ea60026dc24a122a2c
koresachin7 <koresachin7@gmail.com>
2021-10-14 13:50:23 +0530

    Added : Welcome Massege

---
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+/**
+ * Purpose - To implement mood analyser program by handling custom exceptions
+ * @author - Sachin Kore
+ * @version - 16.0
+ */
+public class MoodAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mood Analyser");
+    }
+}
