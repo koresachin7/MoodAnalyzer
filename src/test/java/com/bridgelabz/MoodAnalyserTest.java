d7124bd78a59f94a73fcb70a14eb1123290bfc8a
koresachin7 <koresachin7@gmail.com>
2021-10-15 13:20:08 +0530

    Added : - Null method should throw MoodAnalysisException

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -10,23 +10,48 @@ import org.junit.Test;
 public class MoodAnalyserTest
 {
     @Test
-    public void iAmIn_SadMood_ShouldReturn_sad() throws MoodAnalyserException {
+    public void iAmIn_SadMood_ShouldReturn_Sad() throws MoodAnalyserException
+    {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
-        String mood = moodAnalyser.analyseMood();
+        String  mood = moodAnalyser.analyseMood();
+        try
+        {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalyserException e)
+        {
+            e.printStackTrace();
+        }
         Assert.assertEquals("sad" , mood);
     }
-
     @Test
-    public void iAmIn_anyMood_ShouldReturn_happy() throws MoodAnalyserException {
+    public void iAmIn_anyMood_ShouldReturn_Happy() throws MoodAnalyserException
+    {
+
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
         String mood = moodAnalyser.analyseMood();
+        try
+        {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalyserException e)
+        {
+            e.printStackTrace();
+        }
         Assert.assertEquals("Happy" , mood);
     }
 
     @Test
-    public void null_Should_Return_Happy_Mood() throws MoodAnalyserException {
+    public void when_Null_Should_Return_Message() throws MoodAnalyserException {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("Happy" , mood);
+        try
+        {
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalyserException e)
+        {
+            Assert.assertEquals("Please enter valid message", e.getMessage());
+            System.out.println("Enter valid message");
+        }
+
+
     }
+
 }
